3b0d171f7e6aee679e3f58204041c07ba66cacb4
An Huynh <ahuynh@emailatg.com>
2018-09-06 10:49:07 -0500

    autocomplete?

---
+++ app.js
@@ -0,0 +1,11 @@
+//function ready
+$(function() {
+//on click "send" 
+//take value from each form input
+//append them to the url like: index.html/#values
+
+//var url = window.location.href;
+//console.log(url);
+
+
+});
