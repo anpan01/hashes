806165091c76f2ab26934a323bb04b6a1d6c3e24
An Huynh <ahuynh@emailatg.com>
2018-09-11 14:48:20 -0500

    GOT ITTTT
    bomb dot com

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 //function ready
+var urlwin = window.location.href.toString();
+var exp = RegExp(/[\?|\&]\w+\=([^&\n]*)/, 'g');
+var values;
+var valArray = [];
+
 $(function() {
-  var urlwin = window.location.href.toString();
   console.log(window.location.href);
-  var exp = RegExp(/[\?|\&]\w+\=([^&\n]*)/, 'g');
-  var values;
-  var valArray = [];
   if (urlwin.indexOf('?') > 0) {
     console.log('there is a query string');
     while((values = exp.exec(urlwin)) != null) {
       //switch statement for any other symbol+number parsing
       if (values[1].includes('%20')) {
-        values[1].replace('%20', ' ');
-      }
-      console.log(values)
-      valArray.push(values[1]);
+        console.log('space found');
+        console.log(values[1]);
+        valArray.push(values[1].replace('%20', ' '));
+      } else {
+        valArray.push(values[1]);
+      }     
     }
   }
- 
-  console.log(valArray);
+  //console.log(valArray);
+  $('#pet_name').val(valArray[0]);
+  $('#type').val(valArray[1]);
+  $('#age').val(valArray[2]);
 
   $('#test').on('click', function() {
     window.location.href += "?pet_name=" + $('#pet_name').val() + "&type=" + $('#type').prop('checked')  + "&age=" + $('#age').val(); 
