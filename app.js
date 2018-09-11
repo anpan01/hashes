b878827f3246ab645c125a0b4b5271f3626c21d4
An Huynh <ahuynh@emailatg.com>
2018-09-11 14:22:43 -0500

    woot woot

--- app.js
+++ app.js
@@ -2,18 +2,24 @@
 $(function() {
   var urlwin = window.location.href.toString();
   console.log(window.location.href);
-  var exp = RegExp(/\?\w+\=(\w+|)/, 'g');
+  var exp = RegExp(/[\?|\&]\w+\=([^&\n]*)/, 'g');
   var values;
   var valArray = [];
-  while((values = exp.exec(urlwin)) != null) {
-    console.log(values)
-    valArray.push(values[1]);
+  if (urlwin.indexOf('?') > 0) {
+    console.log('there is a query string');
+    while((values = exp.exec(urlwin)) != null) {
+      //switch statement for any other symbol+number parsing
+      if (values[1].includes('%20')) {
+        values[1].replace('%20', ' ');
+      }
+      console.log(values)
+      valArray.push(values[1]);
+    }
   }
-
+ 
   console.log(valArray);
 
-
   $('#test').on('click', function() {
-    window.location.href += "?pet_name=" + $('#pet_name').val() + "?type=" + $('#type').prop('checked')  + "?age=" + $('#age').val(); 
+    window.location.href += "?pet_name=" + $('#pet_name').val() + "&type=" + $('#type').prop('checked')  + "&age=" + $('#age').val(); 
   });
 });
