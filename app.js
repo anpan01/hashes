9d3a71af79481afd7d9b9aea65bd884c486ba105
An Huynh <ahuynh@emailatg.com>
2018-09-12 15:39:40 -0500

    saving work to switch branches

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ var values;
 var valArray = [];
 
 $(function() {
-  console.log(window.location.href);
   if (urlwin.indexOf('?') > 0) {
-    console.log('there is a query string');
     while((values = exp.exec(urlwin)) != null) {
       //switch statement for any other symbol+number parsing
       if (values[1].includes('%20')) {
-        console.log('space found');
-        console.log(values[1]);
         valArray.push(values[1].replace('%20', ' '));
       } else {
         valArray.push(values[1]);
@@ -25,6 +21,10 @@ $(function() {
   $('#age').val(valArray[2]);
 
   $('#test').on('click', function() {
+    //clear out the old query and replace with the new.
+    if (urlwin.indexOf('?') > 0) {
+      window.location.href = urlwin.slice(urlwin.indexOf('?'));
+    }
     window.location.href += "?pet_name=" + $('#pet_name').val() + "&type=" + $('#type').prop('checked')  + "&age=" + $('#age').val(); 
   });
 });
